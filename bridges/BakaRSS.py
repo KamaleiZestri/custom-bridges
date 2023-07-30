e96f6d8bd49c0468e43e6f0a36898c5a7ebd1f59
KamaleiZestri <38802353+KamaleiZestri@users.noreply.github.com>
2023-07-30 18:16:41 -0500

    Added BakaUpdates and Newgrounds python versions

---
+++ bridges/BakaRSS.py
@@ -0,0 +1,82 @@
+# This script expects one input which is your Baka Updates list such as:
+#   BakaRSS.py "ReadingList.html"
+# Follow these steps to get the HTML file:
+# 1. Go to any Baka Updates List page. ex; Reading List
+# 2. Right click -> "Developer Tools" -> "Inspect"
+# 3. On the new page, copy and paste all the contents into a new HTML file of your own.
+# This scripts outputs JSON feed 1.1: https://jsonfeed.org/version/1.1
+
+import json
+import re
+import sys
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+
+def findID(tag):
+    if tag.find("a") != None:
+        return re.findall(r"/series/([0-9a-zA-Z]*)/", tag.find("a").get("href"))[0]
+    else:
+        return 0
+
+RELEASES_URL:str = "https://www.mangaupdates.com/releases.html"
+
+
+feed:dict = {"items":[]}
+
+feed["title"] = "Baka Updates Manga Releases"
+feed["home_page_url"] = "https://www.mangaupdates.com/"
+feed["description"] = "Get the latest series releases from your follows list"
+
+
+#get all manga on releases page; usually about 3 days back
+releasesHTML:str = urllib.request.urlopen(RELEASES_URL)
+releasesSoup = BeautifulSoup(releasesHTML, "html.parser")
+releasesData = releasesSoup.select("div#main_content div.row > div.pbreak")
+
+releasesList = []
+for i in range(0,len(releasesData),3):
+    releasesList.append([releasesData[i], releasesData[i+1], releasesData[i+2]])
+
+
+#parse html file for list of following manga IDs
+with open(sys.argv[1]) as file:
+# with open("holdem.html") as file:
+    listHTML = file.read()
+listSoup = BeautifulSoup(listHTML, "html.parser")
+listData = listSoup.select("table#ptable tr > td.pl")
+
+idList = []
+for mangaData in listData:
+    idList.append(findID(mangaData))
+
+#search each item in bakaupdates to see if in idlist
+for manga in releasesList:
+    id = findID(manga[0])
+    if not id in idList: continue
+
+    item = {}
+    item["content_html"] = ""
+
+    objTitle = manga[0]
+    if objTitle:
+        title = objTitle.getText()
+        item["content_html"] += "<p>Series: {0} </p>".format(str(objTitle))
+    
+    objVolChap = manga[1]
+    if objVolChap and objVolChap.getText() != "":
+        title += objVolChap.getText()
+
+    objAuthor = manga[2]
+    if objAuthor:
+        item["authors"] = [objAuthor.getText()]
+        item["content_html"] += "<p>Groups: {0} </p>".format(str(objAuthor))
+
+    item["title"] = title
+    item["url"] = "https://www.mangaupdates.com/series/{0}".format(id)
+    item["id"] = hash(title + objAuthor.getText())
+
+    feed["items"].append(item)
+
+print(json.dumps(feed))
---
+++ bridges/NewgroundsRSS.py
@@ -0,0 +1,50 @@
+# This script takes one input which is the username of a Newgrounds user such as:
+#   NewgroundsRSS.py "TomFulp"
+#
+# This scripts outputs JSON feed 1.1: https://jsonfeed.org/version/1.1
+
+import json
+import sys
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+username = sys.argv[1]
+
+feed:dict = {"items":[]}
+
+
+feed["title"] = f"{username} - Newgrounds"
+feed["home_page_url"] = f"https://{username}.newgrounds.com/"
+feed["description"] = f"Get the latest art from {username} on Newgrounds."
+
+artHTML:str = urllib.request.urlopen(f"https://{username}.newgrounds.com/art")
+artSoup = BeautifulSoup(artHTML, "html.parser")
+artData = artSoup.select(".item-portalitem-art-medium")
+
+
+for post in artData:
+    item = {}
+
+    item["authors"] = [username]
+    item["url"] = post.get("href")
+
+    titleOrRestricted = post.select_one("h4").get_text()
+
+    if titleOrRestricted == "Restricted Content: Sign in to view!":
+        item["title"] = f"NSFW: {item['url']}"
+        item["content_html"] = f'<a href="{item["url"]}">{item["title"]}</a>'
+    else:
+        item["title"] = titleOrRestricted
+        item["content_html"] = f"""
+        <a href="{item['url']}">
+        <img
+            style="align:top; width:270px; border:1px solid black;"
+            alt="{item['title']}"
+            src="{post.select_one("img").get("src")}"
+            title="{item['title']}" />
+        </a>
+        """
+    feed["items"].append(item)
+
+print(json.dumps(feed))
